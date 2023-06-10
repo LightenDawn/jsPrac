7ec2039514d9a028d7444d9b5a5ef71ebd234487
Rely <shark90323@gmail.com>
2023-06-11 02:18:39 +0800

    small fix

--- resources/views/prac/test.blade.php
+++ resources/views/prac/test.blade.php
@@ -1,12 +1,6 @@
 @extends('template')
 
 @section('title', 'ORM')
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"
-    integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous">
-</script>
 @include('prac.css.testCss')
 @section('custom_head')
 
@@ -46,7 +40,6 @@
             </li>
         </ul>
     </div>
-
 
     <div class="dropdown">
         <label class="dropdown-desc">Lorem ipsum</label>
